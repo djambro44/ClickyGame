b2314ac3aa4bb038f30793fa44a13d05ded9f872
Dylan Ambrose <djambro44@gmail.com>
2020-07-17 10:27:07 -0700

    Updates

---
+++ precache-manifest.ba2868c60346249a6756e4d74ce06f93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89ba31edf091bff73eabc9f88eb22454",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "2645cdd019780fba7691",
+    "url": "/ClickyGame/static/css/main.2660ec00.chunk.css"
+  },
+  {
+    "revision": "86f63d4e5122677035df",
+    "url": "/ClickyGame/static/js/2.075e05a6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ClickyGame/static/js/2.075e05a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2645cdd019780fba7691",
+    "url": "/ClickyGame/static/js/main.03425085.chunk.js"
+  },
+  {
+    "revision": "7198542e56c22a14260e",
+    "url": "/ClickyGame/static/js/runtime-main.00ef0c1e.js"
+  }
+]);
